d7c9858306c2026049ffc8a1b6ac0907b5bb3c55
shrey <shrey.shreyashh@gmail.com>
2025-02-05 16:24:34 +0530

    functional now

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import random
 
+
 def get_word():
     with open("words.txt", "r") as file:
         allText = file.read()
         words = allText.split()
-    return random.choice(words)   
-
+    return random.choice(words)
 
 
 def underscores():
     for i in randomWord:
         print("_", end=" ")
-    
-randomWord = get_word()
-print(randomWord)
 
 
-stages = ['''
+unformattedRW = get_word()
+randomWord = unformattedRW.lower()
+
+stages = [
+    """
   +---+
   |   |
   O   |
@@ -24,7 +25,8 @@ stages = ['''
  / \\  |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
   O   |
@@ -32,7 +34,8 @@ stages = ['''
  /    |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
   O   |
@@ -40,7 +43,8 @@ stages = ['''
       |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
   O   |
@@ -48,7 +52,8 @@ stages = ['''
       |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
   O   |
@@ -56,7 +61,8 @@ stages = ['''
       |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
   O   |
@@ -64,7 +70,8 @@ stages = ['''
       |
       |
 =========
-''', '''
+""",
+    """
   +---+
   |   |
       |
@@ -72,63 +79,61 @@ stages = ['''
       |
       |
 =========
-''']
-
-
-
-    
+""",
+]
 
 
 def gameloop():
+    print("------------------ Welcome to Hangman by Shrey ------------------")
+    length = len(randomWord)
     stages_length = len(stages)
     chances = 7
     isPlaying = True
     underscores()
-    
+    print(f"\nThe word has {length} letters")
+
     while isPlaying:
         length = len(randomWord)
-        print()
         userInput = str(input("\nEnter guess: "))
-        
+
         if len(userInput) > length:
             print(f"\nTry guessing the word which has {length} letters")
             continue
         elif len(userInput) < length:
             print(f"\nTry guessing the word which has {length} letters")
             continue
-        
-        chances -=1
-        print("\nletter exist in word: ")
+
+        chances -= 1
+        print("letters exist in word: ")
         for i in randomWord:
             if i in userInput:
                 print(i, end=" ")
             else:
                 print("_", end=" ")
-                    
 
-        
-        
         if userInput == randomWord:
             print()
-            print(f"\nYay, you won!! The word was: {randomWord}")
-            isPlaying=False
-            break
-        else:
-            print(f"\nchances left: {chances}")
-            print(stages[stages_length - 1])
-            stages_length -=1
-        
-            
-        if chances == 0:
-            print()
-            print(f"\nYou lost. Do try again")
+            print(f"\nYou won!! The word was: {randomWord}")
             isPlaying = False
             break
-        
-    
+        else:
+            print(stages[stages_length - 1])
+            stages_length -= 1
+
+        if chances == 0:
+            print()
+            print(f"You lost. Do try again")
+            print(f"The word was: {randomWord}")
+            isPlaying = False
+            break
+
+        print(f"Lives: {chances} \n-----------------")
         
 
+
 if __name__ == "__main__":
-    
-    gameloop()
-    
+
+    play_input = str(input("\nDo you want to play(y/n): "))
+
+    if play_input == "y":
+        gameloop()
